bd51573be831a07cabf81b06153e52e54df8ff54
xinghunm <xinghunm@keyayun.com>
2022-09-14 09:37:37 +0000

    feat(T24280): add login error hint (#46)

--- src/views/login-form/with-data/index.tsx
+++ src/views/login-form/with-data/index.tsx
@@ -1,21 +1,57 @@
 import React, { useCallback, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { message } from 'antd';
+import { AxiosError } from 'axios';
+import { useIntl } from 'react-intl';
 
 import { other, user } from 'src/redux';
 import { RoutesMap } from 'src/routes';
 import { ErrorType } from 'src/type';
 import { login } from 'src/api';
 
+type LoginError = AxiosError<{
+  error: {
+    name: string;
+    status: number;
+    message: string;
+  };
+}>;
+
 export const withData =
   <P extends object>(
     WrappedComponent: React.ComponentType<P>,
   ): React.FC<Omit<P, 'onLogin' | 'initialValues' | 'pending'>> =>
   ({ ...props }: any) => {
+    const intl = useIntl();
     const [pending, setPending] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const loginInfo = useSelector(user.selectors.loginInfo);
+
+    const handleError = useCallback(
+      async (error: LoginError) => {
+        setPending(false);
+        const resError = error.response?.data?.error;
+        const blocked = resError?.status === 400 && resError?.message?.includes('has been blocked');
+        if (blocked) {
+          message.warn(intl.formatMessage({ defaultMessage: '该账户为禁用状态，请联系实施同学' }));
+        } else {
+          const validationError = resError?.name === 'ValidationError';
+          if (validationError) {
+            message.error(intl.formatMessage({ defaultMessage: '用户名或密码错误' }));
+          } else {
+            dispatch(
+              other.actions.setError({
+                type: ErrorType.LoginError,
+                detail: (error as Error).message,
+              }),
+            );
+          }
+        }
+      },
+      [dispatch, intl],
+    );
 
     const onLogin = useCallback(
       async (values: { username: string; password: string; remember: boolean }) => {
@@ -28,19 +64,12 @@ export const withData =
 
           navigate(RoutesMap.ROOT);
         } catch (error) {
-          setPending(false);
-          dispatch(
-            other.actions.setError({
-              type: ErrorType.LoginError,
-              detail: (error as Error).message,
-            }),
-          );
+          handleError(error as LoginError);
         }
       },
-      [dispatch, navigate],
+      [dispatch, navigate, handleError],
     );
 
-    console.log('loginInfo', loginInfo);
     return (
       <WrappedComponent
         {...(props as P)}
