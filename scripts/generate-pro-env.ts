6e7be735c82651740427a2c3013b86bd9b9a4b22
xinghunm <xinghunm@keyayun.com>
2022-10-14 14:30:26 +0800

    fix get commit error when build with jekens

--- scripts/generate-pro-env.ts
+++ scripts/generate-pro-env.ts
@@ -1,8 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+import { execSync } from 'child_process';
 
 const generateProEnv = () => {
-  const commitHash = require('child_process').execSync('git rev-parse --short HEAD').toString();
+  execSync(
+    'git config --global --add safe.directory /home/jenkins/agent/workspace/ops-next/ops-next',
+  );
+  const commitHash = execSync('git rev-parse --short HEAD').toString();
   fs.writeFileSync(path.join(__dirname, '../.env.production'), `REACT_APP_VERSION=${commitHash}`);
 };
 generateProEnv();
