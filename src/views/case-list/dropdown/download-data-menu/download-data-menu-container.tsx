9c72835d8e7b396df2210d34b9ba76a30e9f04b6
xinghunm <xinghunm@keyayun.com>
2022-12-02 14:30:05 +0800

    add download data btn

---
+++ src/views/case-list/dropdown/download-data-menu/download-data-menu-container.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useIntl } from 'react-intl';
+
+import { other } from 'src/redux';
+import { NodeStep, ErrorType } from 'src/type';
+import { downloadCaseData } from 'src/utils';
+
+import DownloadDataMenu from './download-data-menu';
+
+interface Props {
+  caseInfo: CaseInfo;
+}
+
+const DownloadDataMenuContainer: React.FC<Props> = (props) => {
+  const intl = useIntl();
+  const { caseInfo } = props;
+  const { caseID, workflowID, step } = caseInfo;
+  const dispatch = useDispatch();
+
+  const disabled = step !== NodeStep.REPORT && step !== NodeStep.RETURNED;
+  const onClick = useCallback(async () => {
+    try {
+      dispatch(
+        other.actions.toggleLoading({
+          visible: true,
+          tip: intl.formatMessage({ defaultMessage: '下载中...' }),
+        }),
+      );
+      await downloadCaseData(caseID, workflowID);
+    } catch (error) {
+      dispatch(
+        other.actions.setError({
+          type: ErrorType.DownloadResultError,
+          detail: (error as Error).message,
+        }),
+      );
+    } finally {
+      dispatch(other.actions.toggleLoading({ visible: false }));
+    }
+  }, [caseID, dispatch, intl, workflowID]);
+
+  return <DownloadDataMenu disabled={disabled} onClick={onClick} />;
+};
+
+export default DownloadDataMenuContainer;
