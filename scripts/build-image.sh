5d22d94abc183851fd4ec0e9b78a7f276d864143
xinghunm <xinghunm@keyayun.com>
2023-01-04 17:21:59 +0800

    enable to throw error when build image

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,6 +1,7 @@
+set -e
 DOCKER_URL=harbor.bj.keyayun.com/xinghunm/ops-next-builder
 make builder-image
 commit=`git describe --dirty --abbrev=8 --tags --always`
 docker tag ${DOCKER_URL} ${DOCKER_URL}:${commit}
 docker push ${DOCKER_URL}:${commit}
-echo build image to ${DOCKER_URL}:${commit}
+echo build image success: ${DOCKER_URL}:${commit}
