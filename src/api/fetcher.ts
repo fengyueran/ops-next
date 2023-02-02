4f6c600e547d17c806bb42d9d3375b96ae69fb2d
xinghunm <xinghunm@keyayun.com>
2023-02-02 17:03:12 +0800

    fix the problem that sometimes the first login fails to get the case

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -4,21 +4,31 @@ export const STRAPI_CMS_HOST = process.env.REACT_APP_STRAPI_CMS_URL || '';
 
 class Fetcher {
   private _axios?: AxiosInstance;
+  private _token?: string;
 
   initAxios = (token: string, onUnauthorizedError: () => void) => {
-    this._axios = A.create({
-      baseURL: STRAPI_CMS_HOST,
-      headers: {
-        authorization: `Bearer ${token}`,
-      },
-    });
-    this._axios.interceptors.response.use(undefined, (error: AxiosError) => {
-      if (error?.response?.status === 401) {
-        onUnauthorizedError();
-      } else {
-        return Promise.reject(error);
-      }
-    });
+    this._token = token;
+    if (!this._axios) {
+      this._axios = A.create({
+        baseURL: STRAPI_CMS_HOST,
+      });
+      this._axios.interceptors.request.use(
+        (config) => {
+          config!.headers!['Authorization'] = `Bearer ${this._token}`;
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
+        },
+      );
+      this._axios.interceptors.response.use(undefined, (error: AxiosError) => {
+        if (error?.response?.status === 401) {
+          onUnauthorizedError();
+        } else {
+          return Promise.reject(error);
+        }
+      });
+    }
   };
 
   get axios(): AxiosInstance {
